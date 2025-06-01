16e21e084a7a07d00c274b58742554b3c201fb0e
Hirofumi Kanda <nandakandahirofumi322@yahoo.co.jp>
2025-06-01 17:47:38 +0900

    右クリックプロパティ表示・ハイライト追加

--- src/main.js
+++ src/main.js
@@ -24,3 +24,89 @@ var map = new maplibregl.Compare(beforeMap, afterMap, container, {});
 afterMap.on("load", async () => {
   afterMap.addControl(new maplibregl.NavigationControl());
 });
+
+function setupContextMenuHandler(map) {
+  map.on("contextmenu", (e) => {
+    const features = map.queryRenderedFeatures(e.point);
+    resetHighlightLayers(map);
+
+    if (features.length > 0) {
+      console.log("フィーチャ数：" + features.length);
+      for (const feature of features) {
+        console.log("レイヤーID：" + feature.layer.id);
+        console.log("フィーチャID：" + feature.id);
+        console.log(JSON.stringify(feature.properties, null, 2));
+      }
+
+      const lineFeatures = features.filter(
+        (f) => "layer" in f && f.layer.type === "line"
+      );
+      if (lineFeatures.length > 0) {
+        map.getSource("highlight-source-line").setData({
+          type: "FeatureCollection",
+          features: lineFeatures,
+        });
+      }
+
+      const fillFeatures = features.filter(
+        (f) => "layer" in f && f.layer.type === "fill" && f.layer.id !== "land"
+      );
+      if (fillFeatures.length > 0) {
+        map.getSource("highlight-source-fill").setData({
+          type: "FeatureCollection",
+          features: fillFeatures,
+        });
+      }
+    }
+  });
+}
+
+function resetHighlightLayers(map) {
+  // Line layer
+  if (map.getSource("highlight-source-line")) {
+    map.removeLayer("highlight-layer-line");
+    map.removeSource("highlight-source-line");
+  }
+  map.addSource("highlight-source-line", {
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: [],
+    },
+  });
+  map.addLayer({
+    id: "highlight-layer-line",
+    type: "line",
+    source: "highlight-source-line",
+    paint: {
+      "line-color": "rgb(255, 0, 0)",
+      "line-width": 2,
+      "line-opacity": 0.8,
+    },
+  });
+
+  // Fill layer
+  if (map.getSource("highlight-source-fill")) {
+    map.removeLayer("highlight-layer-fill");
+    map.removeSource("highlight-source-fill");
+  }
+  map.addSource("highlight-source-fill", {
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: [],
+    },
+  });
+  map.addLayer({
+    id: "highlight-layer-fill",
+    type: "fill",
+    source: "highlight-source-fill",
+    paint: {
+      "fill-outline-color": "rgb(255, 0, 0)",
+    },
+  });
+}
+
+// 両方のマップに適用
+setupContextMenuHandler(beforeMap);
+setupContextMenuHandler(afterMap);
